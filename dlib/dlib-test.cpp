3e31a6bd95433c0a59a47c83e107425323151931
John Scott <johnrscott0@gmail.com>
2018-11-05 21:34:22 +0000

    Fixed a linking problem

--- dlib/dlib-test.cpp
+++ dlib/dlib-test.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <dlib/optimization.h>
 
+// From the optimisation example program
+typedef dlib::matrix<double,0,1> column_vector;
+
 // This the the objective function
 //
 // - The first argument is the set of variables you're optimising
@@ -13,13 +16,39 @@
 //
 // As an example the objective function is
 //
-//   obj = (x[0] - y[0])^2 + (x[1] - y[1])^2
-//       + (x[2] - y[2])^2 + (x[3] - y[3])^2
+//   obj = (x[0] - x[4])^2 + (x[1] - x[5])^2
+//       + (x[2] - x[6])^2 + (x[3] - x[7])^2
 //
-// The x vector is what you're optimising over -- the y vector is
-// a vector of parameters you pass in using f_data.
+// The first four elements of x are what you're optimising over
+// -- the second four are a vector of parameters. NOPE
 //
+double obj(const column_vector & x) {
+  // Calculate the objective function:
+  double value = 0;
+  column_vector y{1,1,1,1};
+  for(int n=0; n<4; n++)
+    value += (x(n) - y(n))*(x(n) - y(n));
+  // Return the value of the function
+  return value;
+}
 
 int main () {
+
+  // The variables of optimisation
+  column_vector x{0,0,0,0};
+  
+  // Do the optimisation -- see example optimisation program
+  find_min_using_approximate_derivatives(dlib::bfgs_search_strategy(),
+					 dlib::objective_delta_stop_strategy(1e-7),
+					 obj, x, -1, 1e-7);
+
+  // Print the result
+  std::cout << "The result of the optimisation is: ["
+	    << x(0) << ", "
+	    << x(1) << ", "
+	    << x(2) << ", "
+	    << x(3) << "] "
+	    << std::endl;
+  
   return 0;
 }
