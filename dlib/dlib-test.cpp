570b60e3086ae8badfe441e0e436ea37395e67ca
John Scott <johnrscott0@gmail.com>
2018-11-06 10:57:57 +0000

    Added parameters to dlib objective function

--- dlib/dlib-test.cpp
+++ dlib/dlib-test.cpp
@@ -16,31 +16,41 @@ typedef dlib::matrix<double,0,1> column_vector;
 //
 // As an example the objective function is
 //
-//   obj = (x[0] - x[4])^2 + (x[1] - x[5])^2
-//       + (x[2] - x[6])^2 + (x[3] - x[7])^2
-//
-// The first four elements of x are what you're optimising over
-// -- the second four are a vector of parameters. NOPE
-//
-double obj(const column_vector & x) {
+//   obj = (x[0] - y[0])^2 + (x[1] - y[1])^2
+//       + (x[2] - y[2])^2 + (x[3] - y[3])^2
+
+double obj(const column_vector & x, const column_vector y) {
   // Calculate the objective function:
   double value = 0;
-  column_vector y{1,1,1,1};
+  //column_vector y{1,1,1,1};
   for(int n=0; n<4; n++)
     value += (x(n) - y(n))*(x(n) - y(n));
   // Return the value of the function
   return value;
 }
 
+// Pass parameters to the objective function
+//double obj_param(const column_vector & x) {
+//  return obj(x, y);
+//}
+
 int main () {
 
   // The variables of optimisation
   column_vector x{0,0,0,0};
+  column_vector y{1,2,3,4};
+
+  // Pass parameters to the objective function (lambda expression)
+  // This page was helpful:
+  // http://www.drdobbs.com/cpp/lambdas-in-c11/240168241
+  auto obj_with_y = [y](column_vector x){
+    return obj(x,y);
+  };
   
   // Do the optimisation -- see example optimisation program
   find_min_using_approximate_derivatives(dlib::bfgs_search_strategy(),
 					 dlib::objective_delta_stop_strategy(1e-7),
-					 obj, x, -1, 1e-7);
+					 obj_with_y, x, -1, 1e-7);
 
   // Print the result
   std::cout << "The result of the optimisation is: ["
